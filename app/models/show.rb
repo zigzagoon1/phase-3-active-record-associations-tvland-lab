727680456cd91cfb9a5eb7375cf9d0944b495730
KellyMark <kmark602@gmail.com>
2023-06-05 18:51:08 -0400

    commit tree 5cd3e315964d0ab6e1c469558b315686a356e227

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list 
+    actors = []
+    self.actors.each do |actor|
+        actors << "#{actor.full_name}"
+    end
+    actors
+  end
   
 end
